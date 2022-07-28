e3607663ee77347de895a616c36ceef4447a72e4
Supawan Pongsupawan <supawan122532@gmail.com>
2022-07-28 21:52:58 +0700

    AddActivities

---
+++ src/Components/AddActivities/AddActivities.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './AddActivities.css';
+
+function AddActivities(props) {
+    return (
+        <div className="AddActivities">
+            <div className="AddActivitiesForm">
+                <form>
+                    <input name="Title" type="text" placeholder="Title" />
+                    <input name="Activities" type="text" placeholder="Activities" />
+                    <input name="username" type="num" placeholder="00:00" />
+
+                    <br />
+                    <div className="description">
+
+                        <textarea placeholder="Description..."name="description" id="" cols="100" rows="100"></textarea>
+                        {/* <input name="Description" type="text" placeholder="Description..." /> */}
+                    
+                    </div>
+                </form>
+            </div >
+
+            <br />
+            <div className="button">
+            <button className="buttonPost">POST</button>
+</div>
+        </div >
+    )
+}
+
+export default AddActivities;
--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './react.svg'
 import Greeting from '../Greeting/Greeting'
+import AddActivities from '../AddActivities/AddActivities'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    <Greeting/>
-          </div>
+      {/* <Greeting/> */}
+      <AddActivities />
+    </div>
   )
 }
 
