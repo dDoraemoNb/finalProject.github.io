9bdb31c2502911fba4c8ab177ae4d7980ac62d1e
Supawan Pongsupawan <supawan122532@gmail.com>
2022-08-20 22:35:12 +0700

    Editprofile v.1

--- src/Components/Editactivities/Editactivities.jsx
+++ src/Components/Editactivities/Editactivities.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './AddActivities.css';
+import './Editactivities.css';
 import DatePicker from "react-datepicker";
 function Editactivities(props) {
     const [startDate, setStartDate] = useState(new Date());
@@ -9,36 +9,38 @@ function Editactivities(props) {
 
                 <form>
                     <div className="AddEdit" >
-                        <div className="info" >
-                            <input name="Title" type="text" placeholder="Title" />
-                            <select name="type_activity" id="dropdown">
-                                <option value=""> -----------</option>
-                                <option value="Running">Running</option>
-                                <option value="Jogging">Jogging</option>
-                                <option value="Hikking">Hikking</option>
-                                <option value="Swimming">Swimming</option>
-                                <option value="Walking">Walking</option>
-                                <option value="Yoga">Yoga</option>
-                            </select>
-                            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} maxDate={new Date()} />
-                            <input name="username" type="time" placeholder="00:00" />
-                        </div>
-                        <br />
-                        <div className="description">
-                            <textarea placeholder="Description..." name="description" id="" cols="180" rows="20"></textarea>
-                            {/* <input name="Description" type="text" placeholder="Description..." /> */}
-                        </div>
+                            <div className="info" >
+                                <input className="Title" name="Title" type="text" placeholder="Title" />
+                                <div className="selectActivity">
+                                    <select name="type_activity" id="dropdown">
+                                        <option value=""> -----------</option>
+                                        <option value="Running">Running</option>
+                                        <option value="Jogging">Jogging</option>
+                                        <option value="Hikking">Hikking</option>
+                                        <option value="Swimming">Swimming</option>
+                                        <option value="Walking">Walking</option>
+                                        <option value="Yoga">Yoga</option>
+                                    </select>
+                                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} maxDate={new Date()} />
+                                    <input name="username" type="time" placeholder="00:00" />
+                                </div>
+                            </div>
+                            <br />
+                            
+                            <div className="description">
+                                <textarea placeholder="Description..." name="description" id="" cols="180" rows="20"></textarea>
+                                {/* <input name="Description" type="text" placeholder="Description..." /> */}
+                            </div>
 
-                    </div>
+                        </div>
                 </form>
 
             </div >
-
-            <br />
-            <div className="button">
-                <button className="buttonPost">SAVE</button>
-                <button className="buttonPost">SUBMIT</button>
-            </div>
+                <br />
+                <div className="button">
+                    <button className="buttonSave">Save</button>
+                    <button className="buttonCancel">Cancel</button>
+                </div>
         </div >
     )
 }
--- src/Components/Editprofile/Editprofile.jsx
+++ src/Components/Editprofile/Editprofile.jsx
@@ -8,16 +8,26 @@ const Editprofile = (props) => {
                 <div className="wrap">
                     <fieldset>
                         <form>
+                            {/* <div className="Title">
+                                <h1><strong>Profile Image Upload</strong></h1>
+                            </div> */}
+
+                            <div className="container-profilepic">
+                                <div className="img">
+                                    {/* <input type="file" /> */}
+                                    <img src="public/profile.png" width={150} />
+                                </div>
+                            </div>
+                            <br />
                             <div className="Title">
                                 <h1><strong>Profile Image Upload</strong></h1>
                             </div>
-                            <div className="img">
-                                {/* <input type="file" /> */}
-                                <img src="public/profile.png" width={150} />
 
-                            </div>
+
                             <div className="AddEditForm">
 
+                                <span className="photo" title="Upload your Avatar!"></span>
+                                <input type="file" className="btn" />
                                 <input name="First Name" type="text" defaultValue={props.profile2.name} />
 
                                 <br />
@@ -27,10 +37,11 @@ const Editprofile = (props) => {
                                 <input name="Email" type="text" defaultValue={props.profile2.email} /> */}
 
                                 <br />
+
                                 <div id="shape">
-                                    <div class='item1'><input id='input1' type="number" defaultValue={props.profile2.yrs} name="yrs" required />Yrs</div>
-                                    <div class='item1'><input id='input1' type="number" defaultValue={props.profile2.weight} name="weight" required />Kg</div>
-                                    <div class='item1'><input id='input1' type="number" defaultValue={props.profile2.height} name="height" required />Cm</div>
+                                    <div className='item1'><input id='input1' type="number" defaultValue={props.profile2.yrs} name="yrs" required />Yrs</div>
+                                    <div className='item1'><input id='input1' type="number" defaultValue={props.profile2.weight} name="weight" required />Kg</div>
+                                    <div className='item1'><input id='input1' type="number" defaultValue={props.profile2.height} name="height" required />Cm</div>
                                     <br />
 
                                 </div>
@@ -46,8 +57,8 @@ const Editprofile = (props) => {
 
                                 <br />
                                 <div id="button">
-                                    <a href='/' ><button className='login1' type="submit" >Save</button></a>
-                                    <a href='/' ><button className='signup1' type="submit" >Submit</button></a>
+                                    <a href='/' ><button className='login1' type="submit" >Save Changes</button></a>
+                                    <a href='/' ><button className='signup1' type="submit" >Cancel</button></a>
                                 </div>
 
                             </div>
